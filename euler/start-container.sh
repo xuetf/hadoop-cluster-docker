29775b2493d0c9fccd8cca2ef8e5a68a542af55f
xuetf <476122294@qq.com>
2020-01-04 11:33:53 +0800

    start-container

--- euler/start-container.sh
+++ euler/start-container.sh
@@ -6,29 +6,29 @@ N=${1:-3}
 image_name='dist_euler:hdfs'
 
 # start hadoop master container
-sudo docker rm -f hadoop-master &> /dev/null
+sudo docker rm -f hadoop-master
 echo "start hadoop-master container..."
 sudo docker run -itd \
                 --net=hadoop \
                 -p 50070:50070 \
                 -p 8088:8088 \
-                -p 8889:8889 \
+                -p 8887:8887 \
                 --name hadoop-master \
                 --hostname hadoop-master \
-                ${image_name} &> /dev/null
+                ${image_name}
 
 
 # start hadoop slave container
 i=1
 while [ $i -lt $N ]
 do
-	sudo docker rm -f hadoop-slave$i &> /dev/null
+	sudo docker rm -f hadoop-slave$i
 	echo "start hadoop-slave$i container..."
 	sudo docker run -itd \
 	                --net=hadoop \
 	                --name hadoop-slave$i \
 	                --hostname hadoop-slave$i \
-	                ${image_name} &> /dev/null
+	                ${image_name}
 	i=$(( $i + 1 ))
 done 
 
